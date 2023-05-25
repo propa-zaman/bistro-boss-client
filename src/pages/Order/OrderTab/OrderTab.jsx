87dc6e9d78cac171f29db6f0bcdadd564ea48928
Afra <propazaman12@gmail.com>
2023-05-25 16:41:30 +0600

    updated

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,10 +1,25 @@
 import FoodCard from '../../../components/FoodCard/FoodCard';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const OrderTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
     return (
         <div >
 
-         
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
                     <div className='grid md:grid-cols-3 gap-10'>
                         {
                             items.map(item => <FoodCard
@@ -14,6 +29,9 @@ const OrderTab = ({ items }) => {
                         }
                     </div>
 
+                </SwiperSlide>
+
+            </Swiper>
         </div>
     );
 };
