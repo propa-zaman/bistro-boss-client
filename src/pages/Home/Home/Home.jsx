90a3e1aba81eeee44bda81576c173f09c6961b22
Afra <propazaman12@gmail.com>
2023-05-25 08:03:10 +0600

    order online

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import useTitle from '../../../hook/useTitle'
+import Category from '../Category/Category';
 
 const Home = () => {
+    useTitle('Home')
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
